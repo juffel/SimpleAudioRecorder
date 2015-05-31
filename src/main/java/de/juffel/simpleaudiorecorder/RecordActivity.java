71e44d8d6b374a13ed80fdf09c9308311be8085a
Julian Dobmann <julian.dobmann@gmail.com>
2015-05-31 18:18:01 +0200

    implemented (unasynchronous) upload method (does not work, since network requests in the main thread are not permitted)

--- src/main/java/de/juffel/simpleaudiorecorder/RecordActivity.java
+++ src/main/java/de/juffel/simpleaudiorecorder/RecordActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -11,7 +13,14 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 // capture Audio Tutorial (via: http://developer.android.com/guide/topics/media/audio-capture.html#audiocapture)
 public class RecordActivity extends Activity {
@@ -35,9 +44,9 @@ public class RecordActivity extends Activity {
         filename += "/record.3gp";
         System.out.println("output recordings to " + filename);
 
+        ///////////////////////////
         // initialize record button
         final Button recordButton = (Button) findViewById(R.id.button_record);
-
         OnClickListener recordListener = new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,9 +65,9 @@ public class RecordActivity extends Activity {
         recordButton.setOnClickListener(recordListener);
         recording = false;
 
+        /////////////////////////
         // initialize play button
         final Button playButton = (Button) findViewById(R.id.button_replay);
-
         OnClickListener playListener = new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -74,10 +83,19 @@ public class RecordActivity extends Activity {
                 playing = !playing;
             }
         };
-
         playButton.setText(R.string.button_replay);
         playButton.setOnClickListener(playListener);
         playing = false;
+
+        ///////////////////////////
+        // initialize upload button
+        final Button uploadButton = (Button) findViewById(R.id.button_upload);
+        uploadButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startUpload();
+            }
+        });
     }
 
     /**
@@ -138,5 +156,53 @@ public class RecordActivity extends Activity {
     private void stopReplay() {
         player.release();
         player = null; // dunno why this is necessary but it appears in the tut, so i adopt it
+    }
+
+    /**
+     * if there is a current recording, this method uploads the file to the for now hardcoded server
+     * via: https://developer.android.com/training/basics/network-ops/connecting.html
+     */
+    private void startUpload() {
+        // retrieve upload file
+        FileInputStream file = null;
+        try {
+            file = new FileInputStream(filename);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // connection stuff
+        OutputStream os = null;
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo ni = cm.getActiveNetworkInfo();
+        if (!(ni != null && ni.isConnected())) {
+            // error
+            System.out.println("Network availability check failed.");
+            return;
+        }
+        System.out.println("Success! Network ok.");
+
+        // proceed
+        try {
+            URL url = new URL("http://127.0.0.1:3000/audio/put_here/");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+
+            conn.connect();
+            os = conn.getOutputStream();
+            os.write(file.read());
+            os.flush();
+            os.close();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
