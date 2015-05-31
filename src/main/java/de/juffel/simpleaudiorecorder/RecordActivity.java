1bb8ac4daf42d5d1704145624bcf279a8a031671
Julian Dobmann <julian.dobmann@gmail.com>
2015-05-31 14:55:17 +0200

    added replay button, added logic for recording and replaying

--- src/main/java/de/juffel/simpleaudiorecorder/RecordActivity.java
+++ src/main/java/de/juffel/simpleaudiorecorder/RecordActivity.java
@@ -1,18 +1,33 @@
 package de.juffel.simpleaudiorecorder;
 
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
 
 
 public class RecordActivity extends ActionBarActivity {
+
+    private static String filename;
+    private MediaRecorder recorder;
+    private MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+
+        // get path for filename
+        Environment.getExternalStorageDirectory().getAbsoluteFile();
+
+
     }
 
     @Override
@@ -38,6 +53,52 @@ public class RecordActivity extends ActionBarActivity {
     }
 
     public void record(View view) {
-        // TODO
+        // change record to stop button
+        Button button = (Button) findViewById(R.id.button_record);
+        button.setText(R.string.button_stop_record);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Button button = (Button) findViewById(R.id.button_record);
+                button.setText(R.string.button_record);
+                // TODO stop and save recording
+                recorder.stop();
+                recorder.release();
+            }
+        });
+
+        // capture Audio (via: http://developer.android.com/guide/topics/media/audio-capture.html#audiocapture)
+        // initialize recorder
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
+        recorder.setOutputFile(filename);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
+
+        try {
+            recorder.prepare();
+            recorder.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void replay(View view) {
+        // change event handler for button
+        Button button = (Button) findViewById(R.id.button_replay);
+        button.setText(R.string.button_stop_replay);
+        /* button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+            }
+        }); */
+
+        player = new MediaPlayer();
+        try {
+            player.setDataSource(filename);
+            player.prepare();
+            player.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
