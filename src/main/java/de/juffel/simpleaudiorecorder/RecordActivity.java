42087023abcccd02b4120531de6f00998ecbd680
Julian Dobmann <julian.dobmann@gmail.com>
2015-05-31 16:32:21 +0200

    Fixed only-once usable Buttons, implemented custom classes for Buttons, temporarily switched to programmatic Layout specification.

--- src/main/java/de/juffel/simpleaudiorecorder/RecordActivity.java
+++ src/main/java/de/juffel/simpleaudiorecorder/RecordActivity.java
@@ -1,28 +1,50 @@
 package de.juffel.simpleaudiorecorder;
 
+import android.app.Activity;
+import android.content.Context;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
-import android.os.Environment;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import java.io.IOException;
 
-
-public class RecordActivity extends ActionBarActivity {
+// capture Audio Tutorial (via: http://developer.android.com/guide/topics/media/audio-capture.html#audiocapture)
+public class RecordActivity extends Activity {
 
     private static String filename;
+
+    private RecordButton recordButton;
+    private PlayButton playButton;
+
     private MediaRecorder recorder;
     private MediaPlayer player;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_record);
+        // create Layout programmatically to easily use custom defined Button-Classes
+        LinearLayout ll = new LinearLayout(this);
+        recordButton = new RecordButton(this);
+        ll.addView(recordButton,
+                new LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        0));
+        playButton = new PlayButton(this);
+        ll.addView(playButton,
+                new LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        0));
+        setContentView(ll);
 
         // get path for filename
         filename = getFilesDir().getAbsolutePath();
@@ -30,30 +52,89 @@ public class RecordActivity extends ActionBarActivity {
         System.out.println("output recordings to " + filename);
     }
 
+    /**
+     * Free the audio record and playback resources so they do not eat battery in background
+     */
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_record, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    protected void onPause() {
+        super.onPause();
+        if (recorder != null) {
+            recorder.release();
+            recorder = null;
         }
 
-        return super.onOptionsItemSelected(item);
+        if (player != null) {
+            player.release();
+            player = null;
+        }
     }
 
-    public void record(View view) {
-        // capture Audio (via: http://developer.android.com/guide/topics/media/audio-capture.html#audiocapture)
+    /**
+     * Custom Subclass of Button (for Record Button), that additionally handles the state of the
+     * Button.
+     */
+    class RecordButton extends Button {
+        Boolean recording;
+
+        OnClickListener clicker = new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (recording) {
+                    setText(R.string.button_record);
+                    stopRecord();
+                } else {
+                    setText(R.string.button_stop_record);
+                    startRecord();
+                }
+                // toggle recording status
+                recording = !recording;
+            }
+        };
+
+        public RecordButton(Context context) {
+            super(context);
+            setText(R.string.button_record);
+            setOnClickListener(clicker);
+            recording = false;
+        }
+    }
+
+    /**
+     * Custom Subclass of Button (for Play Button), that additionally handles the state of the
+     * Button.
+     */
+    class PlayButton extends Button {
+        Boolean playing;
+
+        OnClickListener clicker = new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // toggle play start/stop
+                if (playing) {
+                    setText(R.string.button_replay);
+                    stopReplay();
+                } else {
+                    setText(R.string.button_stop_replay);
+                    startReplay();
+                }
+                // toggle playing status
+                playing = !playing;
+            }
+        };
+
+        public PlayButton(Context context) {
+            super(context);
+            setText(R.string.button_replay);
+            setOnClickListener(clicker);
+            playing = false;
+        }
+    }
+
+
+    /**
+     * Control audio capture (start & stop)
+     */
+    private void startRecord() {
         // initialize recorder
         recorder = new MediaRecorder();
         recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
@@ -67,33 +148,18 @@ public class RecordActivity extends ActionBarActivity {
             e.printStackTrace();
         }
 
-        // change record to stop button
-        Button button = (Button) findViewById(R.id.button_record);
-        button.setText(R.string.button_stop_record);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Button button = (Button) findViewById(R.id.button_record);
-                button.setText(R.string.button_record);
-                recorder.stop();
-                recorder.release();
-            }
-        });
-
         recorder.start();
     }
+    private void stopRecord() {
+        recorder.stop();
+        recorder.release();
+        recorder = null; // dunno why this is necessary but it appears in the tut, so i adopt it
+    }
 
-    public void replay(View view) {
-        // change event handler for button
-        Button button = (Button) findViewById(R.id.button_replay);
-        button.setText(R.string.button_stop_replay);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Button button = (Button) findViewById(R.id.button_replay);
-                button.setText(R.string.button_replay);
-                player.release();
-            }
-        });
-
+    /**
+     * Control audio playback (start & stop)
+     */
+    private void startReplay() {
         player = new MediaPlayer();
         try {
             player.setDataSource(filename);
@@ -102,5 +168,9 @@ public class RecordActivity extends ActionBarActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    private void stopReplay() {
+        player.release();
+        player = null; // dunno why this is necessary but it appears in the tut, so i adopt it
     }
 }
