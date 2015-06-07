a1a6259901cbf1eea3bf0650673d1be687815729
Julian Dobmann <julian.dobmann@gmail.com>
2015-06-08 01:50:14 +0200

    *implemented* ButtonState

--- app/src/main/java/de/juffel/simpleaudiorecorder/ButtonState.java
+++ app/src/main/java/de/juffel/simpleaudiorecorder/ButtonState.java
@@ -1,8 +1,31 @@
 package de.juffel.simpleaudiorecorder;
 
+import android.graphics.drawable.AnimationDrawable;
+
 /**
- * State of a StateButton, managing animations for state entry, during state and state exit.
+ * State of a StateButton, containing animations for state entry and state exit.
  * Created by Julian on 08/06/15.
  */
-public abstract class ButtonState {
+public class ButtonState {
+    private AnimationDrawable enterAnimation, exitAnimation;
+
+    public ButtonState() {
+        // TODO set enter-/exitAnimation
+    }
+
+    public AnimationDrawable getEnterAnimation() {
+        return enterAnimation;
+    }
+
+    public void setEnterAnimation(AnimationDrawable enterAnimation) {
+        this.enterAnimation = enterAnimation;
+    }
+
+    public AnimationDrawable getExitAnimation() {
+        return exitAnimation;
+    }
+
+    public void setExitAnimation(AnimationDrawable exitAnimation) {
+        this.exitAnimation = exitAnimation;
+    }
 }
