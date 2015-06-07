5c813ff162e57fb0fb05c7deb780e739ff3a9cfd
Julian Dobmann <julian.dobmann@gmail.com>
2015-06-08 01:35:32 +0200

    create classes for StateButtons (Buttons that manage and keep track of their states) and ButtonState (Class encapsulating animation of a state).

---
+++ app/src/main/java/de/juffel/simpleaudiorecorder/ButtonState.java
@@ -0,0 +1,8 @@
+package de.juffel.simpleaudiorecorder;
+
+/**
+ * State of a StateButton, managing animations for state entry, during state and state exit.
+ * Created by Julian on 08/06/15.
+ */
+public class ButtonState {
+}
---
+++ app/src/main/java/de/juffel/simpleaudiorecorder/StateButton.java
@@ -0,0 +1,9 @@
+package de.juffel.simpleaudiorecorder;
+
+/**
+ * Custom Button-class that keeps track of its state and manages and displays transition animations
+ * on state changes.
+ * Created by Julian on 08/06/15.
+ */
+public class StateButton {
+}
