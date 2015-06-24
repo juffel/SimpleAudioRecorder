e17b4cf5b68b812695da4aae8d54d2372f496a1e
Julian Dobmann <julian.dobmann@gmail.com>
2015-06-24 11:33:32 +0200

    Dateien werden nun doch wieder in 3gp aufzeichnet, und dann auf dem Server zu mp3 konvertiert.

--- app/src/main/java/de/juffel/simpleaudiorecorder/RecordButton.java
+++ app/src/main/java/de/juffel/simpleaudiorecorder/RecordButton.java
@@ -75,8 +75,8 @@ public class RecordButton extends BasicButton {
         // initialize recorder
         recorder = new MediaRecorder();
         recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        recorder.setOutputFormat(MediaRecorder.OutputFormat.AAC_ADTS);
-        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
         recorder.setOutputFile(file_path);
 
         try {
