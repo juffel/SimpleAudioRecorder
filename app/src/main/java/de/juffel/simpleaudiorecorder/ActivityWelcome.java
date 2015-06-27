d938fca0e8235051362a72e12295d999f1e10486
Julian Dobmann <julian.dobmann@gmail.com>
2015-06-27 19:12:43 +0200

    WelcomeActivity aufgeräumt.

--- app/src/main/java/de/juffel/simpleaudiorecorder/ActivityWelcome.java
+++ app/src/main/java/de/juffel/simpleaudiorecorder/ActivityWelcome.java
@@ -1,14 +1,9 @@
 package de.juffel.simpleaudiorecorder;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.Menu;
-import android.view.MenuItem;
-
-import java.util.concurrent.TimeUnit;
 
 
 public class ActivityWelcome extends ActivityZiegel {
@@ -36,27 +31,5 @@ public class ActivityWelcome extends ActivityZiegel {
                 startActivity(intent);
             }
         }, duration);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_welcome, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
