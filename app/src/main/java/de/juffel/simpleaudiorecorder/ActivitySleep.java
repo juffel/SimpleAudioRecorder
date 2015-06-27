6ca3f87e3da09e12c06b2d0df50bc160f96a7790
Julian Dobmann <julian.dobmann@gmail.com>
2015-06-27 18:43:22 +0200

    Bei Allen Activities (außer Sleep) Keep Screen On auf True gesetzt.

--- app/src/main/java/de/juffel/simpleaudiorecorder/ActivitySleep.java
+++ app/src/main/java/de/juffel/simpleaudiorecorder/ActivitySleep.java
@@ -1,10 +1,15 @@
 package de.juffel.simpleaudiorecorder;
 
 import android.app.Activity;
+import android.app.admin.DevicePolicyManager;
+import android.content.Context;
+import android.os.PowerManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 
 /**
@@ -17,5 +22,15 @@ public class ActivitySleep extends ActivityZiegel {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_activity_sleep, menu);
         return true;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
     }
 }
