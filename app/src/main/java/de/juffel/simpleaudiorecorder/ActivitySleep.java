25b70af0217c940ef2f3a4f4a06cb4201c1b8712
Julian Dobmann <julian.dobmann@gmail.com>
2015-06-27 19:21:34 +0200

    App-Lifecycle ist vollendet :D nach dem Eingeben der Infos auf dem Server erlischt der Code und der Bildschirm schaltet sich nach 15s aus (sieht aber vorher schon aus aus). Beim nächsten Drücken der Entsperr-Taste startet die App von vorne.

--- app/src/main/java/de/juffel/simpleaudiorecorder/ActivitySleep.java
+++ app/src/main/java/de/juffel/simpleaudiorecorder/ActivitySleep.java
@@ -3,6 +3,7 @@ package de.juffel.simpleaudiorecorder;
 import android.app.Activity;
 import android.app.admin.DevicePolicyManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.PowerManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -17,6 +18,8 @@ import android.view.WindowManager;
  */
 public class ActivitySleep extends ActivityZiegel {
 
+    private Boolean first = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,12 +27,19 @@ public class ActivitySleep extends ActivityZiegel {
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-    }
-
-    @Override
     public void onResume() {
         super.onResume();
+
+        // check if this ist the first time on resume is called (on first create) then ignore
+        if (first) {
+            first = false;
+
+        } else {
+        // or the second time then reset application!
+            Intent i = getBaseContext().getPackageManager()
+                    .getLaunchIntentForPackage( getBaseContext().getPackageName() );
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i);
+        }
     }
 }
